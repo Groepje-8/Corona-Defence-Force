cf49016368d28fcc58d7af3295f6aea8fd33843d
JorianBorst <jorianx@live.nl>
2021-03-25 13:09:51 +0100

    ;knoppengoed;

--- Code/src/main/java/nl/han/ica/oopg/game/MenuScherm.java
+++ Code/src/main/java/nl/han/ica/oopg/game/MenuScherm.java
@@ -1,70 +1,77 @@
 package nl.han.ica.oopg.game;
+
 import nl.han.ica.oopg.dashboard.*;
 import nl.han.ica.oopg.objects.GameObject;
 import nl.han.ica.oopg.objects.Sprite;
 import nl.han.ica.oopg.objects.TextObject;
 
-
 public class MenuScherm {
 	private TextObject dashboardText;
 	private Knop instellingenKnop;
+	private Knop startKnop;
 	private Knoppen menuDashboard;
-	private final int xSpelKnop = 0;
-	private final int ySpelKnop = 0;
+	private final int xStartKnop = 0;
+	private final int yStartKnop = 0;
 	private final int xInstellingenKnop = 200;
 	private final int yInstellingenKnop = 200;
 	private final int buttonWidth = 100;
 	private final int buttonHeight = 100;
-	
+
 	private int fontSize, fontSizeInstellingen;
-	
-	
-	public MenuScherm(){
+
+	public MenuScherm() {
 		fontSize = 20;
 		fontSizeInstellingen = 15;
 		createInstellingenKnop();
+		createStartKnop();
 		createDashboard();
 	}
-	
-	public void gaNaarSpelScherm() {
-		
-	}
-	
-	public void gaNaarInstellingenScherm() {
-		
-	}
+
+//	public void gaNaarSpelScherm() {
+//
+//	}
+//
+//	public void gaNaarInstellingenScherm() {
+//
+//	}
+
 	public Knoppen getDashboard() {
 		return menuDashboard;
 	}
-	
+
 	public void createDashboard() {
 		menuDashboard = new Knoppen(0, 0, 1280, 600);
-		menuDashboard.addKnop(instellingenKnop,100);
-	
+		menuDashboard.addKnop(instellingenKnop, 100);
+		menuDashboard.addKnop(startKnop, 100);
 
 	}
+
 	public void createInstellingenKnop() {
 
-		instellingenKnop = new Knop(xInstellingenKnop, yInstellingenKnop, buttonWidth, buttonHeight,
-				"Instellingen");
+		instellingenKnop = new Knop(xInstellingenKnop, yInstellingenKnop, buttonWidth, buttonHeight, "Instellingen");
 		instellingenKnop.setForeColor(146, 20, 12, 255);
 
 	}
+
+	public void createStartKnop() {
+		startKnop = new Knop(xStartKnop, yStartKnop, buttonWidth, buttonHeight, "Start");
+		startKnop.setForeColor(146, 20, 12, 255);
+
+	}
+
 	public Sprite createInstructieSheetBG() {
 		Sprite instructieSheetBG = new Sprite(Spel.MEDIA_URL.concat("instructieSheet.jpg"));
 		return instructieSheetBG;
 	}
-	
+
 	public TextObject[] createInstructieSheet() {
-		TextObject instructieSheet[] = {
-			new TextObject("Instructies!", fontSize),
-			new TextObject("Instructie1", fontSizeInstellingen),
-			new TextObject("Instructie2", fontSizeInstellingen),
-			new TextObject("Instructie3", fontSizeInstellingen),
-			new TextObject("Instructie4", fontSizeInstellingen),
-		};
+		TextObject instructieSheet[] = { new TextObject("Instructies!", fontSize),
+				new TextObject("Instructie1", fontSizeInstellingen),
+				new TextObject("Instructie2", fontSizeInstellingen),
+				new TextObject("Instructie3", fontSizeInstellingen),
+				new TextObject("Instructie4", fontSizeInstellingen), };
 		return instructieSheet;
-	
+
 	}
-	
+
 }
--- Code/src/main/java/nl/han/ica/oopg/game/Spel.java
+++ Code/src/main/java/nl/han/ica/oopg/game/Spel.java
@@ -4,18 +4,14 @@ import java.util.ArrayList;
 
 import nl.han.ica.oopg.dashboard.Dashboard;
 import nl.han.ica.oopg.engine.GameEngine;
-import nl.han.ica.oopg.objects.Sprite;
 import nl.han.ica.oopg.objects.TextObject;
-import nl.han.ica.oopg.tile.GrasTile;
-import nl.han.ica.oopg.tile.TileMap;
-import nl.han.ica.oopg.tile.TileType;
-import nl.han.ica.oopg.tile.WegTile;
 import nl.han.ica.oopg.view.View;
+import nl.han.ica.oopg.game.instellingenScherm;
 
 @SuppressWarnings("serial")
 public class Spel extends GameEngine {
 
-	public static String MEDIA_URL = "src/main/java/media/";
+	public static String MEDIA_URL = "src/main/java/party/game/tutorial/media/";
 
 	private final int MENUSCHERM = 0;
 	private final int INSTELLINGENSCHERM = 1;
@@ -28,18 +24,19 @@ public class Spel extends GameEngine {
 	private TextObject dashboardText;
 	int worldWidth = 1280;
 	int worldHeight = 720;
-	TextObject to;
+
 
 	private int levens, tijd, geld;
-	/*
-	 * private ArrayList<Verdediger> verdedigers; private ArrayList<Vijand> vijand;
-	 * private ArrayList<Projectiel> projectielen; private Map map; private
-	 * Verdediger geselecteerdeVerdediger;
-	 */
+//	private ArrayList<Verdediger> verdedigers;
+//	private ArrayList<Vijand> vijand;
+//	private ArrayList<Projectiel> projectielen;
+//	private Map map;
+//	private Verdediger geselecteerdeVerdediger;
 
 	public static void main(String[] args) {
 		Spel spel = new Spel();
 		spel.runSketch();
+
 	}
 
 	@Override
@@ -50,6 +47,7 @@ public class Spel extends GameEngine {
 		setView(view);
 		size(worldWidth, worldHeight);
 		view.setBackground(30, 30, 36);
+
 		bepaalScherm();
 
 	}
@@ -57,6 +55,18 @@ public class Spel extends GameEngine {
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
+		switch (state) {
+		case MENUSCHERM:
+			
+			dashboard.draw(g);
+			
+			break;
+			
+		case INSTELLINGENSCHERM:
+			dashboard.draw(g);
+			
+			break;
+		}
 
 	}
 
@@ -91,33 +101,6 @@ public class Spel extends GameEngine {
 		}
 	}
 
-	public void addDashboard() {
-		Dashboard dashboard = new Dashboard(0, 0, worldWidth, 300);
-		dashboardText = new TextObject("hi", 12);
-		dashboard.addGameObject(dashboardText);
-		addDashboard(dashboard);
-
-	}
-
-	private void initializeTileMap() {
-
-		Sprite wegSprite = new Sprite(Spel.MEDIA_URL.concat("wegSprite.png"));
-		Sprite grasSprite = new Sprite(Spel.MEDIA_URL.concat("grasSprite.png"));
-
-		TileType<WegTile> wegTileType = new TileType<>(WegTile.class, wegSprite);
-		TileType<GrasTile> grasTileType = new TileType<>(GrasTile.class, grasSprite);
-
-		TileType[] tileTypes = { wegTileType, grasTileType };
-		int tileSize = 64;
-		int tilesMap[][] = { { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 },
-				{ 1, 1, 1, 1, 1, 1, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 1 }, { 1, 1, 1, 1, 1, 1, 1, 1 },
-				{ 1, 1, 1, 1, 1, 1, 1, 1 }, };
-		tileMap = new TileMap(tileSize, tileTypes, tilesMap);
-
-		((WegTile) tileMap.getTileOnIndex(0, 4)).setVolgendeWeg(((WegTile) tileMap.getTileOnIndex(1, 4)));
-
-	}
-
 	public void nieuweGolf() {
 
 	}
@@ -130,6 +113,14 @@ public class Spel extends GameEngine {
 
 	}
 
+	public void addDashboard() {
+		Dashboard dashboard = new Dashboard(0, 0, worldWidth, 300);
+		dashboardText = new TextObject("hi", 12);
+		dashboard.addGameObject(dashboardText);
+		addDashboard(dashboard);
+
+	}
+
 	public void setTileMap() {
 
 	}
@@ -139,23 +130,23 @@ public class Spel extends GameEngine {
 	}
 
 	public void mousePressed() {
-//		if (state == MENUSCHERM) {
-//			if (mouseX > to.getX() && mouseX < (to.getX() + to.getWidth()) && mouseY > to.getY()
-//					&& mouseX < (to.getY() + to.getHeight())) {
-//				state = INSTELLINGENSCHERM;
-//				bepaalScherm();
-//			}
-//		}
 		if (state == MENUSCHERM) {
-			state = INSTELLINGENSCHERM;
-
-			bepaalScherm();
-
-		} else {
-			state = MENUSCHERM;
-
-			bepaalScherm();
+			if (mouseX > to.getX() && mouseX < (to.getX() + to.getWidth()) && mouseY > to.getY()
+					&& mouseX < (to.getY() + to.getHeight())) {
+				state = INSTELLINGENSCHERM;
+				bepaalScherm();
+			}
 		}
+//		if (state == MENUSCHERM) {
+//			state = INSTELLINGENSCHERM;
+//
+//			bepaalScherm();
+//
+//		} else {
+//			state = MENUSCHERM;
+//
+//			bepaalScherm();
+//		}
 
 	}
 
